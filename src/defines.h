174cf319441e29414c2938de4faec379bc7209b4
kounocom <meia@kouno.xyz>
2024-05-06 15:36:44 +0300

    defines

--- src/defines.h
+++ src/defines.h
@@ -26,16 +26,15 @@
 // ================================================
 
 // Set parameters of IMU and board used
-#define IMU IMU_BNO085
-#define SECOND_IMU IMU
-#define BOARD BOARD_SLIMEVR
-#define IMU_ROTATION DEG_270
-#define SECOND_IMU_ROTATION DEG_270
+#define IMU IMU_BMI270
+#define BOARD BOARD_CUSTOM
+#define IMU_ROTATION DEG_90
 
 #define PRIMARY_IMU_OPTIONAL false
-#define SECONDARY_IMU_OPTIONAL true
 
-#define MAX_IMU_COUNT 2
+#define MAX_IMU_COUNT 1
+
+#define ON_OFF_BUTTON 1
 
 // Axis mapping example
 /*
@@ -148,7 +147,22 @@ IMU_DESC_ENTRY(IMU_BMP160, PRIMARY_IMU_ADDRESS_ONE, IMU_ROTATION, PIN_IMU_SCL, P
 //  #define LED_PIN 2
 //  #define LED_INVERTED false
 #elif BOARD == BOARD_CUSTOM
-  // Define pins by the examples above
+  #define PIN_IMU_SDA 10
+  #define PIN_IMU_SCL 4
+  #define PIN_IMU_INT 255
+  #define PIN_IMU_INT_2 255
+  #define PIN_BATTERY_LEVEL 3
+  #define LED_PIN 0
+  #define LED_INVERTED true
+  #ifndef BATTERY_SHIELD_RESISTANCE
+    #define BATTERY_SHIELD_RESISTANCE 0
+  #endif
+  #ifndef BATTERY_SHIELD_R1
+    #define BATTERY_SHIELD_R1 150
+  #endif
+  #ifndef BATTERY_SHIELD_R2
+    #define BATTERY_SHIELD_R2 150
+  #endif
 #elif BOARD == BOARD_WROOM32
   #define PIN_IMU_SDA 21
   #define PIN_IMU_SCL 22
