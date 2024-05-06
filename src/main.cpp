d3f2197cc7226dedf1d34698af563ed2c8f97bf2
kounocom <meia@kouno.xyz>
2024-05-06 15:25:08 +0300

    remove the silly delay

--- src/main.cpp
+++ src/main.cpp
@@ -53,10 +53,10 @@ void setup()
     Serial.begin(serialBaudRate);
     globalTimer = timer_create_default();
 
-#ifdef ESP32C3
-    // Wait for the Computer to be able to connect.
-    delay(2000);
-#endif
+// #ifdef ESP32C3
+//     // Wait for the Computer to be able to connect.
+//     delay(2000);
+// #endif
 
     Serial.println();
     Serial.println();
